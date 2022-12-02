c34c2a4a8d85469e24211c49858c66651ab64185
Shayan Azmoodeh <shkev.az@gmail.com>
2022-12-02 22:57:41 +0000

    update tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,31 +8,35 @@
 #include <queue>
  
 
-TEST_CASE("GraphBuiltCorrectly::Small", "[build-graph]") {
+TEST_CASE("Graph Built Correctly Small", "[build-graph]") {
     Graph g("data/test_small.txt");
-    REQUIRE(g.GetNumEdges() == 5);
+    REQUIRE(g.GetNumEdges() == 6);
     REQUIRE(g.GetNumVertices() == 4);
 }
 
 
-TEST_CASE("GraphBuiltCorrectly::Large", "[build-graph]") {
+TEST_CASE("Graph Built Correctly Large", "[build-graph]") {
     Graph g("data/test_large.txt");
     REQUIRE(g.GetNumEdges() == 25);
     REQUIRE(g.GetNumVertices() == 19);
 }
 
-TEST_CASE("GraphBuiltCorrectly::Main", "[build-graph]") {
+TEST_CASE("Graph Built Correctly Main", "[build-graph]") {
     Graph g("data/wiki-Vote.txt");
     REQUIRE(g.GetNumEdges() == 103689);
     REQUIRE(g.GetNumVertices() == 7115);
 }
 
-// TEST_CASE("")
-
-
-TEST_CASE("VotesDepthAwayFromMostPopular::Small", "[weight=5]") {
+TEST_CASE("Graph::VotesDepthAwayFromMostPopular Small", "[weight=5]") {
     Graph g("data/test_small.txt");
     std::vector<int> nodes_depth = g.VotesDepthAwayFromMostPopular();
-    std::vector<int> ans = {1,3};
+    std::vector<int> ans = {1, 1, 2};
+    REQUIRE(nodes_depth == ans);
+}
+
+TEST_CASE("Graph::ShortestPathFromMostPopular small", "[weight=5]") {
+    Graph g("data/test_small.txt");
+    unordered_map<int, double> nodes_depth = g.ShortestPathFromMostPopular();
+    unordered_map<int, double> ans = {{3, 0}, {1, 1}, {2, 4.0/3}, {4, 4.0/3}};
     REQUIRE(nodes_depth == ans);
 }
