409aca8ce3a39b94ce123a2dcb476dab82d094c5
Patrick Liu <patzliu@gmail.com>
2022-12-12 18:47:43 +0000

    improved graph visualization

--- src/readgraph.py
+++ src/readgraph.py
@@ -5,17 +5,19 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 def read_graph():
+    G = nx.Graph()
     graph_adjacency_list = { }
     for line in open("/Users/dongyu/Desktop/College/cs225/final-project-wiki-vote-groups/adjacency_list.txt"):
-        line = line.rstrip()
-        line = map(int, line.split(" "))
-        graph_adjacency_list.update({ list(line)[0]: { e: 1 for e in list(line)[1:] } })
+        line = line.strip(" ").split(" ")
+        G.add_node(line[0])
+        for i in range(1, len(line)):
+            G.add_edge(line[0], line[i])
+        
 
-    return graph_adjacency_list
+    return G
 
-graph_data = read_graph()
-G = nx.Graph(graph_data)
-nx.draw_networkx(G, with_labels = True, node_color = "c", edge_color = "k", fontsize= 2)
+G = read_graph()
+nx.draw_networkx(G, arrows = True, with_labels = True, node_size = 10, node_color = "c", edge_color = "k", font_size = 1)
 
 plt.axis('off')
 plt.draw()
